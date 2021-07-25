ae4dbc01e8d92c8d458d553281976265d8b6a744
Ronen Amon <ronenamon88@gmail.com>
2021-07-25 23:11:53 +0300

    main file to run

---
+++ Main.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+    Author : ronen amon
+    Main File to run and create instance with start function
+*/
+error_reporting(E_ERROR | E_PARSE);
+
+
+define("IP_ADDRESS","127.0.0.1");
+define("PORT",1111);
+define("IP_TO_PING","8.8.8.8");
+//GOOGLE API KEY
+define("CX","42cf3a15145052826");
+define("KEY","AIzaSyBU2sKQ9trv2SLf1wSvIlbj599lK_WzB9I");
+define("BASE_API_URL","https://www.googleapis.com/customsearch/v1");
+
